565b875328c8ac9b754b54953bf57f816b99aea3
G00405094 <G00405094@atu.ie>
2022-11-29 13:34:25 +0000

    Set up Person CLass

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,4 +1,49 @@
 package ie.atu;
 
 public class Person {
+        private String name;
+        private String address;
+        private String telephone;
+
+    public Person() {
+    }
+
+    public Person(String name, String address, String telephone) {
+        this.name = name;
+        this.address = address;
+        this.telephone = telephone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/ie/atu/PersonApp.java
@@ -0,0 +1,8 @@
+package ie.atu;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Person dave = new Person("Dave","Mervue","12345");
+        System.out.println(dave.toString());
+    }
+}
